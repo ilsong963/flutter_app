4a71f7bfd36107abcc3e1cc52bf97ef66c7509f5
ilsong963 <ilsong963@naver.com>
2020-12-30 23:09:25 +0900

    fire base

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -27,7 +28,38 @@ class UpdateText extends StatefulWidget {
 }
 
 class _UpdateText extends State<UpdateText> {
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
   bool _ischecked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print('token :$token');
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
